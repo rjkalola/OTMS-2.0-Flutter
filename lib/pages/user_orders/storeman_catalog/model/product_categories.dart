28ac7c792e8c3683b11916c71dbb5e19f0d85d0c
Ramu Pidatala <49405215+ramu1561@users.noreply.github.com>
2026-05-08 18:47:55 +0530

    Duplicate category names hidden

--- lib/pages/user_orders/storeman_catalog/model/product_categories.dart
+++ lib/pages/user_orders/storeman_catalog/model/product_categories.dart
@@ -3,18 +3,21 @@ import 'package:belcka/pages/user_orders/storeman_catalog/model/product_info.dar
 class ProductCategories {
   final int categoryId;
   final String categoryName;
+  final bool continuedCategory;
   final List<ProductInfo> products;
 
   ProductCategories({
     required this.categoryId,
     required this.categoryName,
     required this.products,
+    this.continuedCategory = false
   });
 
   factory ProductCategories.fromJson(Map<String, dynamic> json) {
     return ProductCategories(
       categoryId: json['category_id'] ?? 0,
       categoryName: json['category_name'] ?? '',
+      continuedCategory: json['continued_category'] ?? '',
       products: (json['products'] as List?)
           ?.map((e) => ProductInfo.fromJson(e))
           .toList() ??
@@ -26,11 +29,13 @@ class ProductCategories {
   ProductCategories copyWith({
     int? categoryId,
     String? categoryName,
+    bool? continuedCategory,
     List<ProductInfo>? products,
   }) {
     return ProductCategories(
       categoryId: categoryId ?? this.categoryId,
       categoryName: categoryName ?? this.categoryName,
+      continuedCategory: continuedCategory ?? this.continuedCategory,
       products: products ?? this.products,
     );
   }
